f5656ba59f13e3240bfd411ff56d0c19fe91430b
RadoslavIliev89 <radoslaviliev63@gmail.com>
2021-05-26 21:04:08 +0300

    Making request with axios

--- react-section-7/src/components/App.js
+++ react-section-7/src/components/App.js
@@ -1,10 +1,19 @@
 import { Component } from 'react'
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends Component {
 
+    state = { images: [] }
+
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID b5Ldy-DufchgCCtN442_Vbcm4wBrk1IgIIg1X44Z3-w'
+            }
+        })
+            .then((res) => { this.setState({ images: res.date.results }) })
     }
     render() {
         return (
