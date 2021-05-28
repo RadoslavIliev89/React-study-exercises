deba53352beede80ad4045765303bd1439327251
RadoslavIliev89 <radoslaviliev63@gmail.com>
2021-05-28 15:30:05 +0300

    Update Accordion.js

--- react-section-12-widgets/src/components/Accordion.js
+++ react-section-12-widgets/src/components/Accordion.js
@@ -1,23 +1,30 @@
-import { Fragment } from 'react'
-const Accordion = ({ items }) => {
-    const onTitleClick = (index) => {
-        console.log('Title was Clicked',index);
-    }
-    const renderedItems = items.map((x, index) =>
+import { Fragment, useState } from 'react'
 
-        <Fragment key={x.title} >
-            <div className="title active" onClick={() =>onTitleClick(index)}>
+const Accordion = ({ items }) => {
+
+    const [activeIndex, setActiveIndex] = useState('');
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index)
+    }
+
+    const renderedItems = items.map((x, index) =>{
+        const active= index===activeIndex?'active':''
+       return (<Fragment key={x.title} >
+            <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
                 <i className="dropdown icon"></i>
                 {x.title}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                 <p>{x.content}</p>
             </div>
-        </Fragment>
-    )
+        </Fragment>);
+    })
 
     return (
-        <div className="ui styled accordion" style={{ marginTop: '10px', marginLeft: '10px' }}>{renderedItems}</div>
+        <div className="ui styled accordion" style={{ marginTop: '10px', marginLeft: '10px' }}>
+            {renderedItems}
+            </div>
     );
 }
 export default Accordion;
