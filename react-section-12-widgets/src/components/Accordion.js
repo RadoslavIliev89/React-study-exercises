ea1358cff7b7c47f8da5db526b1f1b79f393068d
RadoslavIliev89 <radoslaviliev63@gmail.com>
2021-05-28 12:59:08 +0300

    Added click event for title

--- react-section-12-widgets/src/components/Accordion.js
+++ react-section-12-widgets/src/components/Accordion.js
@@ -1,20 +1,23 @@
+import { Fragment } from 'react'
 const Accordion = ({ items }) => {
+    const onTitleClick = (index) => {
+        console.log('Title was Clicked',index);
+    }
+    const renderedItems = items.map((x, index) =>
 
-    const renderedItems = items.map((x) => 
-
-        <div key={x.title}>
-            <div className="title active">
+        <Fragment key={x.title} >
+            <div className="title active" onClick={() =>onTitleClick(index)}>
                 <i className="dropdown icon"></i>
                 {x.title}
             </div>
             <div className="content active">
                 <p>{x.content}</p>
             </div>
-        </div>
+        </Fragment>
     )
 
     return (
-        <div className="ui styled accordion">{renderedItems}</div>
+        <div className="ui styled accordion" style={{ marginTop: '10px', marginLeft: '10px' }}>{renderedItems}</div>
     );
 }
 export default Accordion;
