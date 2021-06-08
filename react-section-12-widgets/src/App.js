454bea38dc80f426a49d6822f0f5b1b2061f8cea
RadoslavIliev89 <radoslaviliev63@gmail.com>
2021-06-08 14:31:03 +0300

    Added dropdown menu

--- react-section-12-widgets/src/App.js
+++ react-section-12-widgets/src/App.js
@@ -1,27 +1,41 @@
 import Accordion from './components/Accordion';
 import SearchBar from './components/SearchBar';
+import Dropdown from './components/Dropdown';
+import { useState } from 'react';
 
-const items = [
-    {
-        title: 'What is React',
-        content: 'React is a front end JavaScript framework '
-    },
-    {
-        title: 'Why use React',
-        content:'React is a  favorite JS library among engineers'
-    },
-    {
-        title: 'How do you use React',
-        content:'You use React to creating components'
-    }
+// const items = [
+//     {
+//         title: 'What is React',
+//         content: 'React is a front end JavaScript framework '
+//     },
+//     {
+//         title: 'Why use React',
+//         content: 'React is a  favorite JS library among engineers'
+//     },
+//     {
+//         title: 'How do you use React',
+//         content: 'You use React to creating components'
+//     }
+// ]
+
+const options = [
+    { label: 'The Color Red', value: 'red' },
+    { label: 'The Color Green', value: 'green' },
+    { label: 'A Shade of Blue', value: 'blue' }
 ]
 
 
 const App = () => {
 
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            <SearchBar/>
+            <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options}
+            />
         </div>
     );
 }
