62bbfcec68dce79c03de0037dbd68ff9eb9ef38e
RadoslavIliev89 <radoslaviliev63@gmail.com>
2021-06-10 18:49:22 +0300

    Update SearchBar.js

--- videos-app-Hooks/src/components/SearchBar.js
+++ videos-app-Hooks/src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 
 const SearchBar = (props) => {
     const [term, setTerm] = useState('');
@@ -19,31 +19,5 @@ const SearchBar = (props) => {
     );
 }
 
-// class SearchBar extends Component {
 
-    // state = { term: '' }
-
-    // onInputChange = (e) => {
-    //     const term = e.target.value;
-    //     this.setState({ term })
-
-    // }
-    // onFormSubmit = (e) => {
-    //     e.preventDefault();
-    //     this.props.onSubmit(this.state.term)
-    // }
-    // render() {
-
-        // return (
-        //     <div className="ui segment">
-        //         <form className="ui form" onSubmit={this.onFormSubmit}>
-        //             <div className="field">
-        //                 <label>Video Search</label>
-        //                 <input type="text" value={this.state.term} onChange={this.onInputChange} />
-        //             </div>
-        //         </form>
-        //     </div>
-        // );
-    // }
-// }
 export default SearchBar;
