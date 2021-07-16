3113d44aeb9d98dc730b0c7ed3609f3b8398b74f
RadoslavIliev89 <radoslaviliev63@gmail.com>
2021-07-16 14:13:21 +0300

    Added field and button

---
+++ react-section-27/src/components/Field.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class Field extends Component {
+
+    render() {
+        return (
+            <div className="ui filed">
+                <label htmlFor="">Name</label>
+                <input />
+            </div>
+        );
+    }
+
+}
+
+export default Field;
